1f6503f6f076f207c71bb8b80c59329610d92c75
Krstoc1 <94562460+Krstoc1@users.noreply.github.com>
2022-09-08 13:04:22 -0500

    images test 1

--- Unit1/images/sketch.js
+++ Unit1/images/sketch.js
@@ -1,9 +1,10 @@
 let i2, i3;
+let i5, i6;
 let i4;
 
 function setup() {
   createCanvas(500, 500);
-  i4 = loadImage("assests/cow.jpg");
+  i5 = loadImage("assests/cow.jpg");
   i2 = loadImage("assests/sp.jpg");
   i3 = loadImage("assests/spc.jpg");
   imageMode(CENTER);
@@ -11,7 +12,7 @@ function setup() {
 
 function draw() {
   background("grey");
-  image(i4, width/2, height/2, 100, 100);
+  image(i5, width/2, height/2 -120, 100, 100);
   image(i2, width/2, height/2, 100, 100);
   image(i3, width/2, height/2 +120, 100, 100);
 }
