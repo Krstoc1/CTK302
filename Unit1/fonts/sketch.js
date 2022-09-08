91c32da7e7b942fb70e9d6ada4985146a3439e62
Krstoc1 <94562460+Krstoc1@users.noreply.github.com>
2022-09-08 12:59:16 -0500

    fonts test

--- Unit1/fonts/sketch.js
+++ Unit1/fonts/sketch.js
@@ -1,7 +1,18 @@
+let f2, f3;
+let f1;
+
 function setup() {
   createCanvas(500, 500);
+  f2 = loadFont("tes/mlk.ttf");
+  f3 = loadFont("tes/mugs.ttf");
+  textAlign(CENTER);
 }
 
 function draw() {
-
+  background("black");
+  fill("white");
+  textFont(f2,48);
+  text("hello there!", width/2, 100);
+  textFont(f3, 48);
+  text("I am Ironman", width/2, 200);
 }
